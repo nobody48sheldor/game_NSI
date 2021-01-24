63f4c98c3c82f8d9772d5ccb5ce9fae0b9f4d1c9
nobody48sheldor <69438501+nobody48sheldor@users.noreply.github.com>
2021-01-24 05:24:38 +0100

    Update game.py

--- game.py
+++ game.py
@@ -14,6 +14,7 @@ screen.blit(background,(0, 0))
 class game:
     def __init__(self):
         self.player = player()
+        self.rocket = rocket()
         self.key_pressed = {}
 
     def update(self, screen):
@@ -30,6 +31,9 @@ class game:
         if self.key_pressed.get(pygame.K_LEFT) == True:
             self.player.left()
 
+        if rocket.rocket_launch_C == True:
+            screen.blit(self.rocket.image, self.rocket.rect)
+
 class player:
     def __init__(self):
         self.velocity = 5
@@ -37,6 +41,7 @@ class player:
         self.rect = self.image.get_rect()
         self.rect.x = 360
         self.rect.y = 260
+        self.rocket = 5
 
 
     def forward(self):
@@ -56,17 +61,43 @@ class player:
         print("q", self.rect.x, self.rect.y)
 
 
+class rocket:
+    def __init__(self):
+        self.v = 0
+        self.velocity = 50
+        self.image = pygame.image.load('img/rocket.png')
+        self.rect = self.image.get_rect()
+        self.rect.x = game.player.rect.x
+        self.rect.y = game.player.rect.y
+        self.rocket_launch_C = False
+
+    def rocket_launch(self):
+        print(self.v)
+        if self.v < self.velocity:
+            self.rect.x = self.rect.x + (diff_x)
+            self.rect.y = self.rect.y + (diff_y)
+            self.v = self.v + 1
+        if self.v == self.velocity:
+            print("a")
+
+
+
 running = True
 
 game.__init__(game)
 player.__init__(player)
+rocket.__init__(rocket)
 game()
 player()
+rocket()
 
 while running == True:
     screen.blit(background,(0, 0))
     game.update(game, screen)
     pygame.display.flip()
+
+    if rocket.rocket_launch_C == True:
+        game.rocket.rocket_launch()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -77,3 +108,14 @@ while running == True:
             game.key_pressed[event.key] = True
         elif event.type == pygame.KEYUP:
             game.key_pressed[event.key] = False
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            game.player.rocket = game.player.rocket - 1
+            mouse_pos = pygame.mouse.get_pos()
+            player_pos = (game.player.rect.x, game.player.rect.y)
+            diff_x = int((mouse_pos[0] - game.player.rect.x)/rocket.velocity)
+            diff_y = int((mouse_pos[1] - game.player.rect.y)/rocket.velocity)
+            rocket.v = 0
+            rocket.__init__(rocket)
+            rocket.rocket_launch_C = True
+            print("exec")
